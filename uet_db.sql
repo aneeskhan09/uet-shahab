3860794025b5d7192131d3523f373d93e0553484
ANEES <aneeskhan09@gmail.com>
2018-03-17 18:10:32 +0500

    db updated

--- uet_db.sql
+++ uet_db.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 23, 2018 at 10:25 PM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
